34344b5bdd1aea401105df5e7ad30287c327e1f3
Sahadathossain <sahadathossain164109@gmail.com>
2023-02-18 18:45:29 +0600

    added hover

--- js/common.js
+++ js/common.js
@@ -1,10 +1,12 @@
 // Blog Html page go
+
 document.getElementById('blog-btn').addEventListener('click',function(){
   window.location.href = 'blog.html';
 })
 
 
 // input Field Value
+
 function getInputFieldValueById(inputFieldId) {
   const inputField = document.getElementById(inputFieldId);
   const inputFieldValueString = inputField.value;
@@ -46,4 +48,32 @@ function countFunction(countName, countFun) {
       return true;
    }
     
+}
+//   Mouse hover er jnno gogole thake code dakhe idia niah complate krci
+// Mouse hover 
+
+const randomColorValue = document.querySelectorAll(".card-random");
+const originalColors = [];
+
+randomColorValue.forEach(function(element) {
+originalColors.push(element.style.backgroundColor);
+
+element.addEventListener("mouseover", function() {
+  const randomColor = getRandomColor();
+  element.style.backgroundColor = randomColor;
+});
+
+element.addEventListener("mouseout", function() {
+  const index = Array.from(randomColorValue).indexOf(element);
+  element.style.backgroundColor = originalColors[index];
+});
+});
+
+function getRandomColor() {
+const letters = "0123456789ABCDEF";
+let color = "#";
+for (let i = 0; i < 6; i++) {
+  color += letters[Math.floor(Math.random() * 16)];
+}
+return color;
 }
